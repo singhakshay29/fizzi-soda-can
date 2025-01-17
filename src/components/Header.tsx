e97561428c92ab282da70d625c661744415f071f
akshaysingh29 <akshay2898.as@gmail.com>
2025-01-17 22:51:12 +0530

    setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FizziLogo } from './FizziLogo'
+
+const Header = () => {
+  return (
+    <header className='flex justify-center py-4 -mb-28'>
+        <FizziLogo className='h-20 z-10 cursor-pointer text-sky-800'/>
+    </header>
+  )
+}
+
+export default Header
